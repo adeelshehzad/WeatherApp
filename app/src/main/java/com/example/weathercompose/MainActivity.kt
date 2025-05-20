475c8b7ad1af48c8f181ce78afdf23ee23c40985
Adeel Shahzad <adeelshahzed@gmail.com>
2025-05-20 12:48:11 -0400

    Set Navigation routes

--- app/src/main/java/com/example/weathercompose/MainActivity.kt
+++ app/src/main/java/com/example/weathercompose/MainActivity.kt
@@ -1,24 +1,17 @@
 package com.example.weathercompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import com.example.weathercompose.compose.model.Forecast
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
 import com.example.weathercompose.compose.theme.WeatherComposeTheme
-import com.example.weathercompose.compose.view.WeatherScreen
-import com.example.weathercompose.compose.view.WeatherUi
-import com.example.weathercompose.model.Weather
+import com.example.weathercompose.compose.view.MainScreen
 import com.example.weathercompose.network.RetrofitInstance
 import com.example.weathercompose.network.WeatherApiRepositoryImpl
 import com.example.weathercompose.network.WeatherViewModel
 import com.example.weathercompose.network.WeatherViewModelFactory
-import kotlinx.coroutines.flow.FlowCollector
 
 class MainActivity : ComponentActivity() {
     private val weatherViewModel: WeatherViewModel by lazy {
@@ -32,13 +25,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WeatherComposeTheme {
-                Scaffold { innerPadding ->
-                    WeatherScreen(
-                        weatherViewModel = weatherViewModel,
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                WeatherAppEntryPoint(
+                    weatherViewModel = weatherViewModel,
+                )
             }
         }
     }
+}
+
+// Define routes as constants for type safety and easy refactoring
+object AppDestinations {
+    const val LOCATION_SCREEN_ROUTE = "LocationScreen"
+    const val WEATHER_SCREEN_ROUTE = "WeatherScreen"
+    const val SEARCH_SCREEN_ROUTE = "SearchScreen"
+}
+
+@Composable
+fun WeatherAppEntryPoint(weatherViewModel: WeatherViewModel) {
+    val navController = rememberNavController()
+    MainScreen(
+        navHostController = navController,
+        weatherViewModel = weatherViewModel
+    )
 }
