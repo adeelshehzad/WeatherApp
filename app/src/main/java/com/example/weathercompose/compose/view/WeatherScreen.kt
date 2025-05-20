ff84020e896aeabe51152b0dbbd8ae6cee1b85e2
Adeel Shahzad <adeelshahzed@gmail.com>
2025-05-20 12:46:38 -0400

    Added WeatherScreen

---
+++ app/src/main/java/com/example/weathercompose/compose/view/WeatherScreen.kt
@@ -0,0 +1,13 @@
+package com.example.weathercompose.compose.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.weathercompose.network.WeatherViewModel
+
+@Composable
+fun WeatherScreen(
+    weatherViewModel: WeatherViewModel,
+    onNavigateBack: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+}
