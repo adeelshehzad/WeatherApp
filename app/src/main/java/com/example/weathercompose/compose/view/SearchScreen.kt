7d7d578e5fd92d4de3b89194d3e30a183a17b6a5
Adeel Shahzad <adeelshahzed@gmail.com>
2025-05-20 12:46:19 -0400

    Added SearchScreen

---
+++ app/src/main/java/com/example/weathercompose/compose/view/SearchScreen.kt
@@ -0,0 +1,10 @@
+package com.example.weathercompose.compose.view
+
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SearchScreen(modifier: Modifier = Modifier) {
+    HorizontalDivider()
+}
